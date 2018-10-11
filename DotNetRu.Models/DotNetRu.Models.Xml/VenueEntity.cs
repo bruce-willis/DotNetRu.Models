ccdd65a59a3462a911e7fdb4b06d3c86f3e71158
Yury Belousov <yury-belousov@yandex.ru>
2018-10-11 23:15:43 +0300

    add xml project & create nuget package

---
+++ DotNetRu.Models/DotNetRu.Models.Xml/VenueEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace DotNetRu.Models.Xml
+{
+    [XmlType("Venue")]
+    public class VenueEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string MapUrl { get; set; }
+    }
+}
